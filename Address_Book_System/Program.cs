a0967daa8b428b41b9c2cb089e005258d3be9f96
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-12 01:57:27 +0530

    [add] Shubham added UC6 program

--- Address_Book_System/Program.cs
+++ Address_Book_System/Program.cs
@@ -2,15 +2,49 @@
 
 namespace Address_Book_System
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program \n");
 
-            // UC5 : Ability to multiple person to Address Book
-            AddressBook.AddContact();
-            AddressBook.Display();
+            // UC6 : Refactor to add multiple Address Book to the System.Each Address Book has a unique Name
+
+            Console.Write("Enter Number of Address Book you want : ");
+            int numBook = Convert.ToInt32(Console.ReadLine());
+            int numberBook = 0;
+            int key = 1;
+            while (numberBook < numBook)
+            {
+                Console.Write("\n Enter name of Address Book : ");
+                string book = Console.ReadLine();
+                Console.WriteLine("\n Select the below option");
+                Console.WriteLine("1.Add Contact Details \n2.View Contact Details ");
+                Console.Write("Enter Your Choice : ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                while (key != 0)
+                {
+                    switch (num)
+                    {
+
+                        case 1:
+                            AddressBook.AddContact();
+                            break;
+                        case 2:
+                            AddressBook.Details();
+                            break;
+                        default:
+                            Console.Write("Choose correct option : ");
+                            num = Convert.ToInt32(Console.ReadLine());
+                            break;
+                    }
+                    Console.Write("\n Do you wish to continue press 1 or Press O for exit : ");
+                    key = Convert.ToInt32(Console.ReadLine());
+                }
+                AddressBook.AddTo(book);
+                numberBook++;
+            }
         }
     }
 }
