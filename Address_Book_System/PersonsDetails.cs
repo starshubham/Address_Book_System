0dc61e0d65333f5594aaf5d07079dd4eb9d10351
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-11 15:23:39 +0530

    [add] Shubham added Program.cs and PersonsDetails.cs files

---
+++ Address_Book_System/PersonsDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Address_Book_System
+{
+    class PersonsDetails
+    {
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
