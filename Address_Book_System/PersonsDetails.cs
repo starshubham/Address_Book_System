3caa24eb56a3c5e17e0c9659b77b332f8ad5c671
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-13 15:46:18 +0530

    [add] Shubham added UC7 program

--- Address_Book_System/PersonsDetails.cs
+++ Address_Book_System/PersonsDetails.cs
@@ -4,6 +4,7 @@ namespace Address_Book_System
 {
     class Person
     {
+        private string fname, lname, address, city, state, phone, zip, email; //Declaring (Creating) Variables
         public Person(String fname, String lname, String address, String city, String state, String phone, String zip, string email)
         {                                                   //Parameterize constructor
             this.FirstName = fname;
@@ -16,15 +17,15 @@ namespace Address_Book_System
             this.Email = email;
         }
 
-        public string FirstName { get; set; }         // get method returns the value of the variable FirstName.
+        public string FirstName { get => fname; set => fname = value; }         // get method returns the value of the variable FirstName.
                                                       // set method assigns a value to the name variable.
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string PhoneNo { get; set; }
-        public string ZipCode { get; set; }
-        public string Email { get; set; }
+        public string LastName { get => lname; set => lname = value; }
+        public string Address { get => address; set => address = value; }
+        public string City { get => city; set => city = value; }
+        public string State { get => state; set => state = value; }
+        public string PhoneNo { get => phone; set => phone = value; }
+        public string ZipCode { get => zip; set => zip = value; }
+        public string Email { get => email; set => email = value; }
         public override string ToString() //Tostring  method to store value
         {
             return "FirstName:- " + FirstName + "\nLastName:- " + LastName + " \nAddress:- " + Address + " \nCity:- " + City
--- Address_Book_System/Program.cs
+++ Address_Book_System/Program.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 
 namespace Address_Book_System
 {
-    /* UC6:- Refactor to add multiple Address Book to the System.
-                 Each Address Book has a unique Name 
-                 - Use Console to add new Address Book 
-                 - Maintain Dictionary of Address Book Name to Address Book
+    /* UC7:- Ability to ensure there is no Duplicate Entry of the same Person in a particular Address Book
+             - Duplicate Check is done on Person Name while adding person to Address Book.
+             - Use Collection Methods to Search Person by Name for Duplicate Entry
+             - Override equals method to check for Duplicate
+             - Use Lambda
      */
     class Program
     {
